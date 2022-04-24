66e1a32f3b38798cddf529e0c2d9b143cdc15962
[Arian Chauhan s3808369] <s3808369@student.rmit.edu.au>
2022-04-25 02:47:17 +1000

    Converted application to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2022_04_24_085227) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "posts", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
